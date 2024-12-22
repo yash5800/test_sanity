c9ff43292ee0b41a6e4c39be923aa7de671475f2
A.Yaswanth Kiran <yaswanthjk580@gmail.com>
2024-12-22 14:31:09 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://sanityhub.vercel.app', // Replace with your site's URL
+  generateRobotsTxt: true, // Generate a robots.txt file
+};
