4f3a599c84731cdb0773213ad4e7b6930594af13
A.Yaswanth Kiran <yaswanthjk580@gmail.com>
2024-12-29 14:15:14 +0530

    changes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Hero = () => {
   return (
   <>
-    <div className='flex justify-center items-center bg-gray-900 text-gray-600 w-full p-6 gap-4'>
+    <div className='flex justify-center items-center bg-gray-900 text-gray-600 w-full p-6 gap-4 shadow-2xl'>
       <a href='../'><h1 className='max-sm:text-lg text-2xl font-extrabold'>Welcome to <span className='text-blue-700'>SanityHub</span></h1></a>        
       <a href='../'>
       <Image src={'/database.png'} alt='databaselogo' height={35} width={35}/></a>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <img src="/im1.jpg" className="im" alt="back"/>
         {children}
         <Toaster />
       </body>
